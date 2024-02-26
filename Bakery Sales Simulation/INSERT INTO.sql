f6a7858f652e9897ee5047c638216ee5ed9d7554
AlexV <149329570+AlexVikatos@users.noreply.github.com>
2024-02-26 18:54:16 +0200

    Add files via upload

---
+++ Bakery Sales Simulation/INSERT INTO.sql
@@ -0,0 +1,26 @@
+insert into Product values('11','bread',14);
+insert into Product values('12','pastries',14);
+insert into Product values('13','cakes',16);
+insert into Product values('14','cookies',14);
+insert into Product values('15','muffins',20);
+insert into Product values('16','pies',14);
+insert into Product values('17','donuts',13);
+insert into Product values('18','bagels',14);
+insert into Product values('19','biscuits',13);
+insert into Product values('20','rolls',18);
+
+select * from Product
+
+insert into Sales_person values('11','Amir','Woods');
+insert into Sales_person values('12','Megan','Boyer');
+insert into Sales_person values('13','Lorel','Vikatos');
+insert into Sales_person values('14','Elsa','Basiakou');
+insert into Sales_person values('15','Alana','Gounari');
+
+select * from Sales_person
+
+insert into Order_type values('1','Online');
+insert into Order_type values('2','counter');
+insert into Order_type values('3','phone');
+
+select * from Order_type
